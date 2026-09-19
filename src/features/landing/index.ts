e58c91dc700b7adc843a75361dc90ebf56b4e28c
develper21 <githubnarvin@gmail.com>
2026-09-19 12:02:30 +0530

    Add landing feature index

---
+++ src/features/landing/index.ts
@@ -0,0 +1,8 @@
+export { default as LandingPage } from "./components/LandingPage";
+export { LandingNavbar } from "./components/LandingNavbar";
+export { LandingHero } from "./components/LandingHero";
+export { LandingFeatures } from "./components/LandingFeatures";
+export { LandingTelemetryDemo } from "./components/LandingTelemetryDemo";
+export { LandingArchitecture } from "./components/LandingArchitecture";
+export { LandingFAQ } from "./components/LandingFAQ";
+export { LandingFooter } from "./components/LandingFooter";
