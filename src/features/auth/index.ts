92c18cb05a9cae2bd9f7a721d9d583b0cc94d9e6
develper21 <githubnarvin@gmail.com>
2026-09-19 12:02:29 +0530

    Add auth feature index

---
+++ src/features/auth/index.ts
@@ -0,0 +1,5 @@
+export { default as AuthPage } from "./components/AuthPage";
+export { ProtectedRoute } from "./components/ProtectedRoute";
+export { useAuth, AuthProvider } from "./hooks/useAuth";
+export { authService } from "./services/authService";
+export * from "./types/auth.types";
