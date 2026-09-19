6c15d2bde142928e13d20f09549e97aff0428403
develper21 <githubnarvin@gmail.com>
2026-09-19 12:02:29 +0530

    Add JWT token utilities

---
+++ smtrack-backend/src/utils/jwt.ts
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config';
+import { JwtPayload } from '../types';
+
+export const generateAccessToken = (payload: JwtPayload): string => {
+  return jwt.sign(payload, config.jwtSecret, {
+    expiresIn: config.jwtExpiresIn,
+  });
+};
+
+export const generateRefreshToken = (payload: JwtPayload): string => {
+  return jwt.sign(payload, config.jwtRefreshSecret, {
+    expiresIn: config.jwtRefreshExpiresIn,
+  });
+};
+
+export const verifyAccessToken = (token: string): JwtPayload => {
+  return jwt.verify(token, config.jwtSecret) as JwtPayload;
+};
+
+export const verifyRefreshToken = (token: string): JwtPayload => {
+  return jwt.verify(token, config.jwtRefreshSecret) as JwtPayload;
+};
