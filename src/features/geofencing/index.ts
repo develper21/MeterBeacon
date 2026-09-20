ccfb0740831c00e67699542c4980a0d5e21ec9fe
develper21 <githubnarvin@gmail.com>
2026-09-20 18:58:34 +0530

    update geofencing module exports

--- src/features/geofencing/index.ts
+++ src/features/geofencing/index.ts
@@ -1,2 +1,4 @@
 export { default as GeofencingPage } from "./components/GeofencingPage";
+export { GeofenceDetailPage } from "./components/GeofenceDetailPage";
 export { geofenceService } from "./services/geofenceService";
+
